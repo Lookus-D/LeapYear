1bb88091c7bf8561214fa493c6cd63518cb99072
Łukasz Dudek <lukdud@gmail.com>
2023-04-02 23:29:09 +0200

    Add project files.

---
+++ LeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Is it leap year?");
+            Console.WriteLine("Write any year, please.");
+            
+            string year = Console.ReadLine();
+            
+            int chosenYear = 0;
+            
+            Int32.TryParse(year, out chosenYear);
+            if (chosenYear == 2016) 
+            {
+                Console.WriteLine("Yes, 2016 year is leap.");
+            }
+            else
+            {
+                Console.WriteLine($"No, {chosenYear} year is not leap.");
+            }
+        }
+    }
+
+}
